78cd691308b954a40d657ab55ed68904fde67406
JeffRisberg <j.s.risberg@gmail.com>
2022-11-13 17:34:31 -0800

    added two endpoints

--- src/main/java/com/company/udemy/hello/HelloWorldController.java
+++ src/main/java/com/company/udemy/hello/HelloWorldController.java
@@ -3,12 +3,18 @@ package com.company.udemy.hello;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Put;
 
-@Controller("/hello")
+@Controller("/")
 public class HelloWorldController {
 
-    @Get(produces = MediaType.TEXT_PLAIN)
+    @Get(uri="/hello", produces = MediaType.TEXT_PLAIN)
     public String helloWorld() {
-        return "hello World";
+        return "Hello World!";
+    }
+
+    @Get(uri="/goodbye", produces = MediaType.TEXT_PLAIN)
+    public String goodbyeWorld() {
+        return "Goodbye World!";
     }
 }
